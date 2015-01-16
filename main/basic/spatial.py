968d7befe86972bd75adc59a98da2155663c066c
policratus <policratus@gmail.com>
2015-01-15 23:48:21 -0200

    New spatial transformations and parsing command line arguments.

--- main/basic/spatial.py
+++ main/basic/spatial.py
@@ -2,8 +2,24 @@ from PIL import Image
 from basic import io
 
 class spatial():
+    fio = io.io() 
+
     def rotation(self,filepath,degrees=90):
-        fio = io.io() 
-        image = fio.open(filepath) 
+        image = self.fio.open(filepath) 
         rotated = image.rotate(degrees)
         return rotated
+
+    def resize(self,filepath,width=640,height=480):
+        image = self.fio.open(filepath)
+        resized = image.resize((width,height))
+        return resized
+
+    def colorspace(self,filepath,space):
+        image = self.fio.open(filepath)
+
+        if space == 'greyscale':
+            newspace = image.convert('L')
+        else:
+            newspace = None
+
+        return newspace
